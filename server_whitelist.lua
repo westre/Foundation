be87239e33a5205228474925900d39cf19b60169
Kevin de Korte <kevdekorte@hotmail.com>
2015-11-25 18:32:01 +0100

    Added whitelist.txt file reading Added basic command handling

--- server_whitelist.lua
+++ server_whitelist.lua
@@ -1,12 +1,15 @@
 local configuration = {
-	["whitelisted_names"] = {
-		"user1",
-		"user2",
-		"user3",
-		"user4" -- etc, just make sure the last entry does not have a comma
-	}
+	["whitelisted_names"] = {}
 }
 
+local whitelistConfig = './resources/westre-whitelist/whitelist.txt'
+
+-- This event gets called when the resource is starting
+AddEventHandler('onResourceStarting', function(resource)
+	refreshWhitelist()
+end)
+
+-- Custom event gets called when a client joins
 RegisterServerEvent('foundation:onPlayerConnect')
 AddEventHandler('foundation:onPlayerConnect', function()
     local isWhitelisted = false
@@ -16,7 +19,6 @@ AddEventHandler('foundation:onPlayerConnect', function()
 	
 	for key, value in ipairs(configuration["whitelisted_names"]) do
 		if configuration["whitelisted_names"][key] == name then
-			print("Is a whitelisted player!")
 			isWhitelisted = true;
 			break;
 		end
@@ -24,3 +26,43 @@ AddEventHandler('foundation:onPlayerConnect', function()
 	
 	TriggerClientEvent('foundation:playerWhitelisted', source, isWhitelisted)
 end)
+
+-- Hook into the chat event to handle messages (requires chat resource)
+AddEventHandler('chatMessageEntered', function(name, color, message)
+    if message:sub(1, 1) == '!' and string.len(message) >= 2 then
+		print("Executed command: " .. message .. " by " .. name)
+		
+		if message == "!refreshwhitelist" then
+			refreshWhitelist()
+			
+			local namesWhitelisted = ""
+			for key, value in ipairs(configuration["whitelisted_names"]) do
+				namesWhitelisted = namesWhitelisted .. configuration["whitelisted_names"][key] .. " | "
+			end
+			
+			TriggerClientEvent('chatMessage', source, '', { 255, 255, 255 }, "Whitelist has been refreshed: " .. namesWhitelisted)
+		end
+	end
+end)
+
+-- Refreshes the whitelist table
+function refreshWhitelist() 
+	print("Refreshing whitelist...")
+	-- Clear the whitelist table
+	for key, value in ipairs(configuration["whitelisted_names"]) do 
+		configuration["whitelisted_names"][key] = nil 
+	end
+	
+	local file = io.open(whitelistConfig, "rb")
+  	if file then 
+		for line in io.lines(whitelistConfig) do 
+			-- Populate the whitelist table with contents from the textfile
+			table.insert(configuration["whitelisted_names"], line)
+  		end	
+  		file:close() 
+	else
+		print("Error: can not locate whitelist.txt")
+	end
+	
+	print("Found " .. #configuration["whitelisted_names"] .. " whitelisted entries")
+end
